4783c0b02bdbacbf7aba52a3ac28d5063ef9698b
Mahliyo <gasandigital13@gmail.com>
2026-06-08 21:31:05 +0500

    hero star

--- src/Components/Main/Hero/Hero.jsx
+++ src/Components/Main/Hero/Hero.jsx
@@ -1,6 +1,12 @@
 import { useTranslation } from "react-i18next";
 import "./Hero.css";
 import React from 'react'
+import heror from "./image/heror.png";
+import vector1 from "./image/vector1.png";
+import vector2 from "./image/vector2.png";
+import vector3 from "./image/vector3.png";
+import vector4 from "./image/vector4.png";
+
 
 function Hero() {
     const [t, i18n] = useTranslation();
@@ -14,7 +20,36 @@ function Hero() {
                         <p className="hero-text">{t("hero.hero-text")}</p>
                          <button className="hero-btn">{t("hero.hero-btn")} <span className="hero-span"></span></button>
                     </div>
+                    <div className="hero-right">
+                        <img src={heror} alt="rasm" />
+                    </div>
                 </div>
+                <ul className="hero-list">
+                    <li className="hero-item">
+                        <p className="heroList-text">{t("hero.heroList-text")}</p>
+                        <h3 className="heroList-title">{t("hero.heroList-title")}</h3>
+                    </li>
+                    <li className="hero-item">
+                        <img src={vector1} alt="rasm" />
+                         <p className="heroList-text">{t("hero.heroList-text1")}</p>
+                    </li>
+                    <li className="hero-item">
+                        <img src={vector2} alt="rasm" />
+                         <p className="heroList-text">{t("hero.heroList-text1")}</p>
+                    </li>
+                    <li className="hero-item">
+                        <img src={vector3} alt="rasm" />
+                         <p className="heroList-text">{t("hero.heroList-text1")}</p>
+                    </li>
+                    <li className="hero-item">
+                        <img src={vector2} alt="rasm" />
+                         <p className="heroList-text">{t("hero.heroList-text1")}</p>
+                    </li>
+                    <li className="hero-item">
+                        <img src={vector4} alt="rasm" />
+                         <p className="heroList-text">{t("hero.heroList-text1")}</p>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
