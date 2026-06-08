7e3a2116343bd3b100aacb2f23016a21d5b27533
Mahliyo <gasandigital13@gmail.com>
2026-06-08 17:05:39 +0500

    My Projects style

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route>
+        <Route path="home" element = {}/>
+      </Route>
+    </Routes>
+
+       </BrowserRouter>
+    
+    </>
+  )
+}
+
+
+export default App
