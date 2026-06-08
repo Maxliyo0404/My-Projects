b21f247bb4dbeba2025e95dc4336e09d27c90958
Mahliyo <gasandigital13@gmail.com>
2026-06-08 22:52:20 +0500

    sectionOne wrapper

--- src/Components/Main/SectionOne/SectionOne.jsx
+++ src/Components/Main/SectionOne/SectionOne.jsx
@@ -1,6 +1,10 @@
 import { useTranslation } from "react-i18next";
 import "./SectionOne.css";
 import React from 'react'
+import { FaLongArrowAltRight } from "react-icons/fa";
+import one111 from "./image/one111.png";
+import one222 from "./image/one222.png";
+import one333 from "./image/one333.png";
 
 function SectionOne() {
     const [t, i18n] = useTranslation();
@@ -22,9 +26,9 @@ function SectionOne() {
                    
                 </div>
                 <div className="one-rasms">
-                    <img className="one-rasm" src={} alt="rasm" />
-                    <img className="one-rasm" src={} alt="rasm" />
-                    <img className="one-rasm" src={} alt="rasm" />
+                    <img className="one-rasm" src={one111} alt="rasm" />
+                    <img className="one-rasm" src={one222} alt="rasm" />
+                    <img className="one-rasm" src={one333} alt="rasm" />
                 </div>
                 <div className="one-lists">
                <ul className="one-list">
@@ -65,7 +69,7 @@ function SectionOne() {
                    <p className="oneList-text">{t("sectionOne.oneList-text4")}</p>
                  </li>
                </ul>
-               <a className="one-link" href="#"><span className="one-span"></span></a>
+               <a className="one-link" href="#">{t("sectionOne.one-link")}<span className="one-span"><FaLongArrowAltRight /></span></a>
             </div>
             </div>
         </div>
