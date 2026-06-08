6015dd4099aa41949bc7c84a7829a29ad99d5f7a
Mahliyo <gasandigital13@gmail.com>
2026-06-08 17:41:08 +0500

    My Projects layot

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,6 +2,8 @@ import "./Header.css";
 import React from 'react'
 import i18next from "i18next";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+L
 
 function Header() {
     const [t, i18n] = useTranslation();
@@ -11,12 +13,13 @@ function Header() {
             <div className="header-container">
                 <a className="logo" href="#"></a>
                 <ul className="header-list">
-                    <li><a className="header-link" href="#"></a></li>
-                    <li><a className="header-link" href="#"></a></li>
-                    <li><a className="header-link" href="#"></a></li>
-                    <li><a className="header-link" href="#"></a></li>
-                    <li><a className="header-link" href="#"></a></li>
-                    <li><a className="header-link" href="#"></a></li>
+                    <li><Link className="header-link" to="home">{t("header.home")}</Link></li>
+                    <li><Link className="header-link" to="services">{t("header.services")}</Link></li>
+                    <li><Link className="header-link" to="company">{t("header.company")}</Link></li>
+                    <li><Link className="header-link" to="career">{t("header.career")}</Link></li>
+                    <li><Link className="header-link" to="blog">{t("header.blog")}</Link></li>
+                    <li><Link className="header-link" to="contact us">{t("header.contact us")}</Link></li>
+                    <li><Link className="header-link" to=" clone project">{t("header.clone project")}</Link></li>
                 </ul>
             </div>
         </div>
