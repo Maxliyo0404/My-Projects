3088a4942683c218db5a141a7692aa425cdd42b9
Mahliyo <gasandigital13@gmail.com>
2026-06-08 17:16:12 +0500

    My Projects style css

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePages from "./Pages/HomePages/HomePages";
 import ServicesPages from "./Pages/ServicesPages/ServicesPages";
 import CompanyPages from "./Pages/CompanyPages/CompanyPages";
+import Layout from "./Components/Layout/Layout";
 
 function App() {
   return (
     <>
     <BrowserRouter>
     <Routes>
-      <Route>
+      <Route element={<Layout/>}>
         <Route path="home" element = {<HomePages/>}/>
         <Route path="services" element = {<ServicesPages/>}/>
         <Route path="company" element = {<CompanyPages/>}/>
 
       </Route>
     </Routes>
-
        </BrowserRouter>
     
     </>
