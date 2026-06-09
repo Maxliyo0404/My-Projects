0d2e91c8e6d1b446569b2c16edf45e97831296ef
Mahliyo <gasandigital13@gmail.com>
2026-06-09 19:01:20 +0500

    sectionTwo responsive fnish

--- src/Components/Main/SectionThree/SectionThree.jsx
+++ src/Components/Main/SectionThree/SectionThree.jsx
@@ -8,7 +8,9 @@ function SectionThree() {
     <div className="sectionThree">
         <div className="container">
             <div className="sectionThree-wrapper">
-                
+                 <h6 className="three-title">{t("sectionThree.three-title")}</h6>
+                     <h3 className="three-title1">{t("sectionThree.three-title1")}</h3>
+                     <button className="tthree-btn">{t("sectionThree.three.btn")}</button>
             </div>
         </div>
     </div>
