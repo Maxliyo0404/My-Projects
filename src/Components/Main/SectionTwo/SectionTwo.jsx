3bd939dbebbb3859053c86c6908cc9d22ad70f0a
Mahliyo <gasandigital13@gmail.com>
2026-06-09 16:26:59 +0500

    sectionOne wrapper css responsive fnish

---
+++ src/Components/Main/SectionTwo/SectionTwo.jsx
@@ -0,0 +1,18 @@
+import { useTranslation } from "react-i18next";
+import "./SectionTwo.css";
+import React from 'react'
+
+function SectionTwo() {
+    const [t, i18n] = useTranslation();
+  return (
+    <div className="sectionTwo">
+        <div className="container">
+            <div className="sectionTwo-wrapper">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SectionTwo
--- src/Pages/HomePages/HomePages.jsx
+++ src/Pages/HomePages/HomePages.jsx
@@ -2,12 +2,14 @@ import "./HomePages.css";
 import React from 'react'
 import Hero from "../../Components/Main/Hero/Hero";
 import SectionOne from "../../Components/Main/SectionOne/SectionOne";
+import SectionTwo from "../../Components/Main/SectionTwo/SectionTwo";
 
 function HomePages() {
   return (
     <div>
         <Hero/>
         <SectionOne/>
+        <SectionTwo/>
     </div>
   )
 }
