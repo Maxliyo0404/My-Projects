e4a54d05f3cbfd7d2f4ff41e81197cef84f0dce8
Mahliyo <gasandigital13@gmail.com>
2026-06-10 22:19:29 +0500

    sectinSix star fnish

--- src/Components/Main/SectionSeven/SectionSeven.jsx
+++ src/Components/Main/SectionSeven/SectionSeven.jsx
@@ -8,7 +8,29 @@ function SectionSeven() {
     <div className="sectionSeven">
         <div className="container">
             <div className="sectionSeven-wrapper">
-                
+                <div className="decor-box">
+            <div className="d-orange"></div>
+            <div className="d-light-orange"></div>
+            <div className="d-blue"></div>
+          </div>
+
+          <div className="newsletter-content">
+            <div className="newsletter-text">
+              <span className="subtitle">NEWSLETTER</span>
+              <h2>Subscribe our News Letter to get Latest Updates.</h2>
+            </div>
+            
+            <div className="newsletter-input">
+              <input type="email" placeholder="Email@domain.com" />
+              <button>Subscribe</button>
+            </div>
+          </div>
+
+          <div className="side-bar">
+            <div className="s-orange"></div>
+            <div className="s-light-orange"></div>
+            <div className="s-blue"></div>
+          </div>
             </div>
         </div>
     </div>
