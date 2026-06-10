b7981ddc3e858b19db2fc8f229984ad7269baf95
Mahliyo <gasandigital13@gmail.com>
2026-06-10 18:53:44 +0500

    sectionFour star

--- src/Components/Main/SectionFour/SectionFour.jsx
+++ src/Components/Main/SectionFour/SectionFour.jsx
@@ -11,41 +11,43 @@ function SectionFour() {
     <div className="sectionFour">
       <div className="container">
         <div className="sectionFour-wrapper">
-          {/* Chap qism: Matnlar */}
           <div className="four-left">
             <h2 className="four-title">{t("sectionFour.title")}</h2>
             <p className="four-text">{t("sectionFour.text")}</p>
             <p className="four-texte">{t("sectionFour.four-text")}</p>
             <div className="four-imgs">
+              <div className="four-img">
               <img src={fourP_1} alt="rasm" />
               <img src={fourP_2} alt="rasm" />
               <img src={fourP_3} alt="rasm" />
-              <h3 className="four-title">{t("sectionFour.four-title")}</h3>
+              </div>
+              <h3 className="four-titlee">{t("sectionFour.four-title")}</h3>
             </div>
           </div>
 
-          {/* O'ng qism: Kard va vertikal chiziq */}
-          <div className="four-right">
-            <div className="testimonial-card">
-              <div className="vertical-bar">
-                <div className="v-orange"></div>
-                <div className="v-light-orange"></div>
-                <div className="v-blue"></div>
-              </div>
-              <div className="card-content">
-                <p className="card-quote">
-                  {t("sectionFour.quote")}
-                </p>
-                <div className="card-author">
-                    <img src={fourP_1} alt="rasm" />
-                  <div className="author-info">
-                    <h4>{t("sectionFour.name")}</h4>
-                    <span>{t("sectionFour.role")}</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+         <div className="four-right">
+  <div className="testimonial-card">
+    <div className="vertical-bar">
+      <div className="v-orange"></div>
+      <div className="v-light-orange"></div>
+      <div className="v-blue"></div>
+    </div>
+    
+    <div className="card-content">
+      <div className="quote-icon">,,</div> 
+      
+      <p className="card-quote">{t("sectionFour.quote")}</p>
+      
+      <div className="card-author">
+        <img src={fourP_1} alt="Author" className="author-img" />
+        <div className="author-info">
+          <h4>{t("sectionFour.name")}</h4>
+          <span>{t("sectionFour.role")}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
         </div>
       </div>
     </div>
