eb22cf27888e95f484dc456535cd02b7442347ad
Mahliyo <gasandigital13@gmail.com>
2026-06-10 21:32:13 +0500

    sectionSix star

--- src/Components/Main/SectionSix/SectionSix.jsx
+++ src/Components/Main/SectionSix/SectionSix.jsx
@@ -2,7 +2,7 @@ import { useTranslation } from "react-i18next";
 import "./SectionSix.css";
 import React from 'react'
 import sixP_1 from "./image/sixP_1.png";
-import sixP_1 from "./image/sixP_1.png";
+import sixP_2 from "./image/sixP_2.png";
 
 function SectionSix() {
     const [t, i18n] = useTranslation();
