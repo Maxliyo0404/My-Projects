55bec102f26146d8a166c73e764704b3b3aa6343
Mahliyo <gasandigital13@gmail.com>
2026-06-10 22:01:38 +0500

    sectionSix star item css responsive

---
+++ src/Components/Main/SectionSeven/SectionSeven.jsx
@@ -0,0 +1,12 @@
+import { useTranslation } from "react-i18next";
+import "./SectionSeven.css";
+import React from 'react'
+
+function SectionSeven() {
+    const [t, i18n] = useTranslation();
+  return (
+    <div>SectionSeven</div>
+  )
+}
+
+export default SectionSeven
