e691f6086bbeff339e038888eddce7728a03cd89
Mahliyo <gasandigital13@gmail.com>
2026-06-10 21:41:33 +0500

    sectionSix star list

--- src/Components/Main/SectionSix/SectionSix.jsx
+++ src/Components/Main/SectionSix/SectionSix.jsx
@@ -3,7 +3,7 @@ import "./SectionSix.css";
 import React from 'react'
 import sixP_1 from "./image/sixP_1.png";
 import sixP_2 from "./image/sixP_2.png";
-
+import { FaLongArrowAltRight } from "react-icons/fa";
 function SectionSix() {
     const [t, i18n] = useTranslation();
   return (
@@ -18,7 +18,7 @@ function SectionSix() {
                         <div className="six-right">
                              <p className="sixList-text">{t("sectionSix.sixList-text")}</p>
                          <h4 className="sixList-title">{t("sectionSix.sixList-title1")}</h4>
-                          <a className="sixList-link" href="#">{t("sectionSix.sixList-link")}<span className="sixList-span"></span></a>
+                          <a className="sixList-link" href="#">{t("sectionSix.sixList-link")}<span className="sixList-span"><FaLongArrowAltRight /></span></a>
                         </div>
                     </li>
                     <li className="six-item">
@@ -26,7 +26,7 @@ function SectionSix() {
                         <div className="six-right">
                              <p className="sixList-text">{t("sectionSix.sixList-text")}</p>
                          <h4 className="sixList-title">{t("sectionSix.sixList-title2")}</h4>
-                          <a className="sixList-link" href="#">{t("sectionSix.sixList-link")}<span className="sixList-span"></span></a>
+                          <a className="sixList-link" href="#">{t("sectionSix.sixList-link")}<span className="sixList-span"><FaLongArrowAltRight /></span></a>
                         </div>
                     </li>
                     
