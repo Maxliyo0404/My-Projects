6977aa0bad8720bb5c296ed6dda9f5fb5befc47b
Mahliyo <gasandigital13@gmail.com>
2026-06-11 00:22:48 +0500

    sectionSeven wrapper css

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -2,13 +2,14 @@ import "./Layout.css";
 import React from 'react'
 import Header from "../Header/Header";
 import { Outlet } from "react-router-dom";
-
+import Footer from "../../Components/Footer/Footer";
 function Layout() {
   return (
     <div className="layout">
         <main>
             <Header/>
             <Outlet/>
+            <Footer/>
         </main>
     </div>
   )
