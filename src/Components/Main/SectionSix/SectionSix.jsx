17fc0be3909cef4f6250ed929e747fa778f09887
Mahliyo <gasandigital13@gmail.com>
2026-06-10 19:52:39 +0500

    sectionFour star wrapper responsive

---
+++ src/Components/Main/SectionSix/SectionSix.jsx
@@ -0,0 +1,12 @@
+import { useTranslation } from "react-i18next";
+import "./SectionSix.css";
+import React from 'react'
+
+function SectionSix() {
+    const [t, i18n] = useTranslation();
+  return (
+    <div>SectionSix</div>
+  )
+}
+
+export default SectionSix
