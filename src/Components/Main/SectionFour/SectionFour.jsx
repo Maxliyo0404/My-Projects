7fabce4c97361d1a19e4fa2b0ea99d6681fd769f
Mahliyo <gasandigital13@gmail.com>
2026-06-10 17:43:41 +0500

    sectionFive fnish

--- src/Components/Main/SectionFour/SectionFour.jsx
+++ src/Components/Main/SectionFour/SectionFour.jsx
@@ -1,8 +1,9 @@
 import { useTranslation } from "react-i18next";
 import "./SectionFour.css";
 import React from 'react';
-import avatar1 from "../../assets/images/avatar1.png"; // Avatar rasmlaringiz yo'li
-
+import fourP_1 from "./image/fourP_1.png";
+import fourP_2 from "./image/fourP_2.png";
+import fourP_3 from "./image/fourP_3.png";
 function SectionFour() {
   const { t } = useTranslation();
 
@@ -12,9 +13,15 @@ function SectionFour() {
         <div className="sectionFour-wrapper">
           {/* Chap qism: Matnlar */}
           <div className="four-left">
-            <h6 className="four-subtitle">{t("sectionFour.subtitle")}</h6>
             <h2 className="four-title">{t("sectionFour.title")}</h2>
             <p className="four-text">{t("sectionFour.text")}</p>
+            <p className="four-texte">{t("sectionFour.four-text")}</p>
+            <div className="four-imgs">
+              <img src={fourP_1} alt="rasm" />
+              <img src={fourP_2} alt="rasm" />
+              <img src={fourP_3} alt="rasm" />
+              <h3 className="four-title">{t("sectionFour.four-title")}</h3>
+            </div>
           </div>
 
           {/* O'ng qism: Kard va vertikal chiziq */}
@@ -30,7 +37,7 @@ function SectionFour() {
                   {t("sectionFour.quote")}
                 </p>
                 <div className="card-author">
-                  <img src={avatar1} alt="Author" className="author-img" />
+                    <img src={fourP_1} alt="rasm" />
                   <div className="author-info">
                     <h4>{t("sectionFour.name")}</h4>
                     <span>{t("sectionFour.role")}</span>
