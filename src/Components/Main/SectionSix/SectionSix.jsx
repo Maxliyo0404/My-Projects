631075c86b9bb1f1bf3a80a87f403d3200e3d2e3
Mahliyo <gasandigital13@gmail.com>
2026-06-10 20:04:33 +0500

    sectionFour fnish

--- src/Components/Main/SectionSix/SectionSix.jsx
+++ src/Components/Main/SectionSix/SectionSix.jsx
@@ -8,7 +8,26 @@ function SectionSix() {
     <div className="sectionSix">
         <div className="container">
             <div className="sectionSix-wrapper">
-                
+                <div className="six-box"></div>
+                 <h2 className="six-title">{t("sectionSix.six-title")}</h2>
+                 <ul className="six-list">
+                    <li className="six-item">
+                        <img src={} alt="rasm" />
+                    </li>
+                    <li className="six-item">
+                         <p className="sixList-text">{t("sectionSix.sixList-text")}</p>
+                         <h4 className="sixList-text">{t("sectionSix.sixList-title1")}</h4>
+                          <a className="sixList-link" href="#">{t("sectionSix.sixList-link")}<span className="sixList-span"></span></a>
+                    </li>
+                    <li className="six-item">
+                        <img src={} alt="rasm" />
+                    </li>
+                     <li className="six-item">
+                         <p className="sixList-text">{t("sectionSix.sixList-text")}</p>
+                         <h4 className="sixList-text">{t("sectionSix.sixList-title2")}</h4>
+                          <a className="sixList-link" href="#">{t("sectionSix.sixList-link")}<span className="sixList-span"></span></a>
+                    </li>
+                 </ul>
             </div>
         </div>
     </div>
