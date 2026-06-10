c5b9410afb88398da83a3962b6eb4989d68f3e10
Mahliyo <gasandigital13@gmail.com>
2026-06-10 17:08:47 +0500

    sectionFive css  responsive

---
+++ src/Components/Main/SectionFour/SectionFour.jsx
@@ -0,0 +1,12 @@
+import { useTranslation } from "react-i18next";
+import "./SectionFour.css";
+import React from 'react'
+
+function SectionFour() {
+    const [t, i18n] = useTranslation();
+  return (
+    <div>SectionFour</div>
+  )
+}
+
+export default SectionFour
