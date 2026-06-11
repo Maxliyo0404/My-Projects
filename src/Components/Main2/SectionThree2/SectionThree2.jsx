268ad550f0d88e4c9a885d6bbc6555c97be80b05
Mahliyo <gasandigital13@gmail.com>
2026-06-11 16:51:00 +0500

    sectionThree2 star wrapper

--- src/Components/Main2/SectionThree2/SectionThree2.jsx
+++ src/Components/Main2/SectionThree2/SectionThree2.jsx
@@ -1,11 +1,27 @@
 import { useTranslation } from "react-i18next";
 import "./SectionThree2.css";
 import React from 'react'
+import three2_1 from "./image/three2_1.png"
 
 function SectionThree2() {
     const [t, i18n] = useTranslation();
   return (
-    <div>SectionThree2</div>
+      <div className="sectionThree2">
+           <div className="container">
+             <div className="sectionThree2-wrapper">
+                 <div className="three2-left">
+                 <img className="two2-rasm" src={three2_1} alt="rasm" />
+               </div>
+               <div className="three2-right">
+                   <h4 className="three2-title">{t("sectionThree2.three2-title")}</h4>
+                   <h2 className="three2-titlee">{t("sectionThree2.three2-titlee")}</h2>
+                   <p className="three2-text">{t("sectionThree2.three2-text")}</p>
+               </div>
+              
+              
+             </div>
+           </div>
+         </div>
   )
 }
 
