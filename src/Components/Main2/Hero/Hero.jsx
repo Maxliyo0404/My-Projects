1c77af808bbb420aefde6870f681d20557ef0735
Mahliyo <gasandigital13@gmail.com>
2026-06-11 15:13:48 +0500

    Main2 hero star

--- src/Components/Main2/Hero/Hero.jsx
+++ src/Components/Main2/Hero/Hero.jsx
@@ -8,7 +8,16 @@ function Hero() {
     <div className="hero">
         <div className="container">
             <div className="hero-info">
-                
+                <div className="hero-left">
+                    <h6 className="hero-title">{t("hero-hero-title")}</h6>
+                    <h1 className="hero-title1">{t("hero-hero-title1")}</h1>
+                     <p className="hero-text">{t("hero.hero-text")}</p>
+                      <button className="hero-btn">
+                        <span className="yellow-box"></span> 
+                        {t("hero-btn")} 
+                        <span className="hero-span"><FaLongArrowAltRight /></span>
+                    </button>
+                </div>
             </div>
         </div>
     </div>
