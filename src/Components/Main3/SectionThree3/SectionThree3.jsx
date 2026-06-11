bfc7060433e9860620c2f83024c298f927eb4a92
Mahliyo <gasandigital13@gmail.com>
2026-06-12 00:08:37 +0500

    sectionTwo3 star fnish

--- src/Components/Main3/SectionThree3/SectionThree3.jsx
+++ src/Components/Main3/SectionThree3/SectionThree3.jsx
@@ -1,14 +1,17 @@
 import { useTranslation } from "react-i18next";
 import "./SectionThree3.css";
 import React from 'react'
-
+import three3_1 from "./image/three3_1.png";
 function SectionThree3() {
     const [t, i18n] = useTranslation();
   return (
     <div className="sectionThree3">
         <div className="container">
             <div className="sectionThree-wrapper">
-                
+                        <h6 className="three3-label">{t("sectionThree3.three3-label")}</h6>
+                        <h2 className="three3-title">{t("sectionThree3.three3-title")}</h2>
+                        <p className="three3-text">{t("sectionThree3.three3-text")}</p>
+                        <img src={three3_1} alt="rasm" />
             </div>
         </div>
     </div>
