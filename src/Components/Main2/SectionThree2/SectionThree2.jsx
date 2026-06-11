fc5d00686cb52fa0cf60982c4806e092799769be
Mahliyo <gasandigital13@gmail.com>
2026-06-11 16:42:38 +0500

    sectionThree2 star

---
+++ src/Components/Main2/SectionThree2/SectionThree2.jsx
@@ -0,0 +1,12 @@
+import { useTranslation } from "react-i18next";
+import "./SectionThree2.css";
+import React from 'react'
+
+function SectionThree2() {
+    const [t, i18n] = useTranslation();
+  return (
+    <div>SectionThree2</div>
+  )
+}
+
+export default SectionThree2
--- src/Pages/ServicesPages/ServicesPages.jsx
+++ src/Pages/ServicesPages/ServicesPages.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Hero2 from "../../Components/Main2/Hero2/Hero2";
 import SectionFive from "../../Components/Main/SectionFive/SectionFive";
 import SectionTwo2 from "../../Components/Main2/SectionTwo2/SectionTwo2";
+import SectionThree2 from "../../Components/Main2/SectionThree2/SectionThree2";
 
 
 
@@ -12,6 +13,7 @@ function ServicesPages() {
      <Hero2/>
      <SectionFive/>
      <SectionTwo2/>
+     <SectionThree2/>
     </div>
   )
 }
