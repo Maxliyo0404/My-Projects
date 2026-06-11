31c3c8f5c508aef6dd4cfe678d05ecbad9bad154
Mahliyo <gasandigital13@gmail.com>
2026-06-11 18:29:59 +0500

    sectionFive2 star css responsive fnish

---
+++ src/Components/Main2/SectionSix2/SectionSix2.jsx
@@ -0,0 +1,12 @@
+import { useTranslation } from "react-i18next";
+import "./SectionSix.css";
+import React from 'react'
+
+function SectionSix2() {
+    const [t, i18n] = useTranslation();
+  return (
+    
+  )
+}
+
+export default SectionSix2
