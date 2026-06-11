2cc548a466bf4e6dac60972d76ea1f2a5ed71b31
Mahliyo <gasandigital13@gmail.com>
2026-06-11 17:05:02 +0500

    sectionFour2 star

---
+++ src/Components/Main2/SectionFour2/SectionFour2.jsx
@@ -0,0 +1,25 @@
+import { useTranslation } from "react-i18next";
+import "./SectionFour2.css";
+import React from 'react'
+
+function SectionFour2() {
+    const [t, i18n] = useTranslation();
+  return (
+      <div className="sectionFour2">
+           <div className="container">
+             <div className="sectionFour2-wrapper">
+               <div className="four2-left">
+                   <h4 className="four2-title">{t("sectionFour2.four2-title")}</h4>
+                   <h2 className="four2-titlee">{t("sectionFour2.four2-titlee")}</h2>
+                   <p className="four2-text">{t("sectionFour2.four2-text")}</p>
+               </div>
+               <div className="four2-right">
+                 <img className="four2-rasm" src={} alt="rasm" />
+               </div>
+             </div>
+           </div>
+         </div>
+  )
+}
+
+export default SectionFour2
--- src/Pages/ServicesPages/ServicesPages.jsx
+++ src/Pages/ServicesPages/ServicesPages.jsx
@@ -4,6 +4,7 @@ import Hero2 from "../../Components/Main2/Hero2/Hero2";
 import SectionFive from "../../Components/Main/SectionFive/SectionFive";
 import SectionTwo2 from "../../Components/Main2/SectionTwo2/SectionTwo2";
 import SectionThree2 from "../../Components/Main2/SectionThree2/SectionThree2";
+import SectionFour2 from "../../Components/Main2/SectionFour2/SectionFour2";
 
 
 
@@ -14,6 +15,7 @@ function ServicesPages() {
      <SectionFive/>
      <SectionTwo2/>
      <SectionThree2/>
+     <SectionFour2/>
     </div>
   )
 }
