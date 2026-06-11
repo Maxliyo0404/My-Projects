b65ad128e6c4b5703d60ee3db2eec2a6145d49c8
Mahliyo <gasandigital13@gmail.com>
2026-06-11 19:21:48 +0500

    3 pages star

---
+++ src/Components/Main3/Hero3/Hero3.jsx
@@ -0,0 +1,18 @@
+import { useTranslation } from "react-i18next";
+import "./Hero3.css";
+import React from 'react'
+
+function Hero3() {
+    const [t, i18n] = useTranslation();
+  return (
+    <div className="hero">
+        <div className="container">
+            <div className="hero-info">
+              
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero3
--- src/Pages/CompanyPages/CompanyPages.jsx
+++ src/Pages/CompanyPages/CompanyPages.jsx
@@ -1,9 +1,11 @@
 import "./CompanyPages.css";
 import React from 'react'
-
+import Hero3 from "../../Components/Main3/Hero3/Hero3";
 function CompanyPages() {
   return (
-    <div>CompanyPages</div>
+    <>
+    <Hero3/>
+    </>
   )
 }
 
