6ee62c3bb5a5c6f7453228f75835a123c9dc74b7
Mahliyo <gasandigital13@gmail.com>
2026-06-11 17:28:35 +0500

    sectionFive star

--- src/Components/Main2/SectionFive2/SectionFive2.jsx
+++ src/Components/Main2/SectionFive2/SectionFive2.jsx
@@ -5,7 +5,22 @@ import React from 'react'
 function SectionFive2() {
     const [t, i18n] = useTranslation();
   return (
-    
+     <div className="sectionFive2">
+                <div className="container">
+                  <div className="sectionFive2-wrapper">
+                      <div className="five2-left">
+                      <img className="five2-rasm" src={three2_1} alt="rasm" />
+                    </div>
+                    <div className="five2-right">
+                        <h4 className="five2-title">{t("sectionFive2.five2-title")}</h4>
+                        <h2 className="five2-title">{t("sectionFive2.five2-titlee")}</h2>
+                        <p className="five2-text">{t("sectionFive2.five2-text")}</p>
+                    </div>
+                   
+                   
+                  </div>
+                </div>
+              </div>
   )
 }
 
