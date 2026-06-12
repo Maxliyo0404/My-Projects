fb06b51851ccf72cd2c0a02859cb40628ef867a0
Mahliyo <gasandigital13@gmail.com>
2026-06-12 05:41:27 +0500

    sectionFour3 star

--- src/Pages/CompanyPages/CompanyPages.jsx
+++ src/Pages/CompanyPages/CompanyPages.jsx
@@ -4,7 +4,7 @@ import Hero3 from "../../Components/Main3/Hero3/Hero3";
 import SectionTwo3 from "../../Components/Main3/SectionTwo3/SectionTwo3";
 import SectionTwo from "../../Components/Main/SectionTwo/SectionTwo";
 import SectionThree3 from "../../Components/Main3/SectionThree3/SectionThree3";
-import
+import SectionFour3 from "../../Components/Main3/SectionFour3/SectionFour3";
 function CompanyPages() {
   return (
     <>
