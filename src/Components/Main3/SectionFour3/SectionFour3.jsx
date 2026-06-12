57e26a7d70f328bd862b6f593db83b6d16bbe212
Mahliyo <gasandigital13@gmail.com>
2026-06-12 05:44:53 +0500

    sectionFour3 star text

--- src/Components/Main3/SectionFour3/SectionFour3.jsx
+++ src/Components/Main3/SectionFour3/SectionFour3.jsx
@@ -8,9 +8,9 @@ function SectionFour3() {
     <div className="sectionFour3">
         <div className="container">
             <div className="sectionFour3-wrapper">
-                <h6 className="four3-label">{t("sectionThree3.three3-label")}</h6>
-                <h2 className="four3-title">{t("sectionThree3.three3-title")}</h2>
-                <p2 className="four3-text">{t("sectionThree3.three3-text")}</p>
+                <h6 className="four3-label">{t("sectionFour3.four3-label")}</h6>
+                <h2 className="four3-title">{t("sectionFour3.four3-title")}</h2>
+                <p className="four3-text">{t("sectionFour3.four3-text")}</p>
             </div>
         </div>
     </div>
