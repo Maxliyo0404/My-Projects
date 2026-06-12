c2b89f01c4ce4721c6e68e6d8c7e79ba6f81e677
Mahliyo <gasandigital13@gmail.com>
2026-06-12 05:40:47 +0500

    sectionFour3 star wrapper

--- src/Components/Main3/SectionFour3/SectionFour3.jsx
+++ src/Components/Main3/SectionFour3/SectionFour3.jsx
@@ -1,8 +1,9 @@
+import { useTranslation } from "react-i18next";
 import "./SectionFour3.css";
 import React from 'react'
 
 function SectionFour3() {
-    const
+    const [t, i18n] = useTranslation();
   return (
     <div>SectionFour3</div>
   )
