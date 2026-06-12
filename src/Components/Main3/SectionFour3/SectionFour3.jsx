9e62cfc1fbe370b736f150847d71527503fda0ee
Mahliyo <gasandigital13@gmail.com>
2026-06-12 05:42:44 +0500

    sectionFour3 star title

--- src/Components/Main3/SectionFour3/SectionFour3.jsx
+++ src/Components/Main3/SectionFour3/SectionFour3.jsx
@@ -5,7 +5,9 @@ import React from 'react'
 function SectionFour3() {
     const [t, i18n] = useTranslation();
   return (
-    <div className="sectionFour3"></div>
+    <div className="sectionFour3">
+        <div className="container"></div>
+    </div>
   )
 }
 
