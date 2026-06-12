36e2fe389ff143ea8b5b0705930a794d9989296b
Mahliyo <gasandigital13@gmail.com>
2026-06-12 05:46:07 +0500

    sectionFour3 star text css

--- src/Components/Main3/SectionFour3/SectionFour3.jsx
+++ src/Components/Main3/SectionFour3/SectionFour3.jsx
@@ -11,7 +11,11 @@ function SectionFour3() {
                 <h6 className="four3-label">{t("sectionFour3.four3-label")}</h6>
                 <h2 className="four3-title">{t("sectionFour3.four3-title")}</h2>
                 <p className="four3-text">{t("sectionFour3.four3-text")}</p>
-                <ul className="four3-list"></ul>
+                <ul className="four3-list">
+                    <li className="four3-item">
+                        <img src="" alt="" />
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
